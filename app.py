58cf6947f5b304465a05cd1eb3518b3bbacce46f
shiue2000 <shiue2000@gmail.com>
2025-06-28 14:56:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -214,4 +214,5 @@ def index():
     return render_template('index.html', original_url=original_url, enhanced_url=enhanced_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
